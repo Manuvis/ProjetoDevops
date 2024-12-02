50e4c36624d6bdb24b27f6c8a19d9b385736abff
Manuvis <manuellafigueiredojatoba@gmail.com>
2024-12-02 19:33:42 -0300

    projeto parte 2

---
+++ backend/src/main/java/com/todolist/to_do_list/config/SimpleCORSFilter.java
@@ -0,0 +1,56 @@
+package com.todolist.to_do_list.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SimpleCORSFilter implements Filter {
+
+    private final List<String> allowedOrigins = Arrays.asList(
+            "http://localhost/80",
+            "http://localhost:3000",
+            "https://98.84.157.19",
+            "https://98.84.157.19/3000"
+    );
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        String origin = request.getHeader("Origin");
+
+
+        if (origin != null && allowedOrigins.stream().anyMatch(origin::equalsIgnoreCase)) {
+
+            response.setHeader("Access-Control-Allow-Origin", origin);
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+        }
+
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+
+        // Handle preflight requests (OPTIONS)
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
